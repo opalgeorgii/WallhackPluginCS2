305cdd15de84137c0f00bd698d2803eb124bb748
Heorhii Vainer <opalgeorgii@gmail.com>
2026-03-24 06:20:59 +0200

    changed invisibilitydata to glowdata to match the class name

---
+++ src/Models/GlowData.cs
@@ -0,0 +1,9 @@
+using CounterStrikeSharp.API.Core;
+
+namespace Funnies.Models;
+
+public class GlowData
+{
+    public required CDynamicProp GlowEnt { get; init; }
+    public required CDynamicProp ModelRelay { get; init; }
+}
--- src/Models/InvisibleData.cs
+++
@@ -1,9 +0,0 @@
-using CounterStrikeSharp.API.Core;
-
-namespace Funnies.Models;
-
-public class GlowData
-{
-    public required CDynamicProp GlowEnt;
-    public required CDynamicProp ModelRelay;
-}

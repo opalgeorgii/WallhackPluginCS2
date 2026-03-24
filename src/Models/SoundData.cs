cfb6864e31a862e0f31288d56d1cb2923eaf730e
Heorhii Vainer <opalgeorgii@gmail.com>
2026-03-24 06:26:18 +0200

    updated SoundData.cs

--- src/Models/SoundData.cs
+++ src/Models/SoundData.cs
@@ -1,8 +1,15 @@
 namespace Funnies.Models;
 
-public struct SoundData(float startTime = -1f, float endTime = -1f)
+public struct SoundData
 {
-    public float StartTime = startTime;
-    public float EndTime = endTime;
-    public bool HackyReload = false;
+    public float StartTime { get; set; }
+    public float EndTime { get; set; }
+    public bool HackyReload { get; set; }
+
+    public SoundData(float startTime = -1f, float endTime = -1f)
+    {
+        StartTime = startTime;
+        EndTime = endTime;
+        HackyReload = false;
+    }
 }
